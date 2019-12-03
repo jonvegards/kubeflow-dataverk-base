3b4afe5a223ef54547f2d758eef9b41b63a354ec
Vattekar <42180319+erikvatt@users.noreply.github.com>
2019-12-03 10:29:06 +0100

    set APPLICATION_NAME env in docker-entrypoint script

--- notebooks/docker-entrypoint.sh
+++ notebooks/docker-entrypoint.sh
@@ -1,10 +1,6 @@
-BUCKET_ENDPOINT_FILE=${VKS_SECRET_DEST_PATH}/DATAVERK_BUCKET_ENDPOINT
+#!/bin/bash
+set -e
 
-if test -f "$BUCKET_ENDPOINT_FILE"; then
-    export DATAVERK_BUCKET_ENDPOINT=$(cat $BUCKET_ENDPOINT_FILE)
-fi
-
-export EDITOR='nano'
-#export DATAVERK_SETTINGS_PATH='/home/jovyan'
+export APPLICATION_NAME=$(cat "${K8S_SERVICEACCOUNT_PATH}/namespace")
 
 jupyter notebook --notebook-dir=/home/${NB_USER} --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=${NB_PREFIX}
