506dee89c427422fcc282f031e31895112588692
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-02-18 15:37:47 +0100

    add local image

---
+++ notebooks/jupyterlab/local/docker-entrypoint-jupyterlab.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+
+jupyter lab --notebook-dir=/home/${NB_USER} --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=${NB_PREFIX}
